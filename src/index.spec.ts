ecdfefc2cf01777e7a5f97cfa48ca882bcfee305
Junyoung Choi <fluke8259@gmail.com>
2019-07-09 19:57:28 +0900

    Implement middleware test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -24,7 +24,7 @@ test('integration test', async t => {
     }
   }
 
-  await testServer([sessionMiddleware, Handler] as any, async url => {
+  await testServer([sessionMiddleware, Handler], async url => {
     const postResponse = await got.post(url, {
       cookieJar
     })
@@ -37,3 +37,45 @@ test('integration test', async t => {
     t.is(getResponse.body, 'Hello, World!')
   })
 })
+
+test('middleware sets sid', async t => {
+  const cookieJar = new CookieJar()
+  const { Session, sessionMiddleware } = createSession({
+    store: new MemoryStore(),
+    secret: ''
+  })
+  class Handler {
+    async handle(@Method() method: string, @Session() session: SessionStore) {
+      if (method === 'POST') {
+        session.update({ message: 'Hello, World!' })
+        return 'OK'
+      } else {
+        const data = session.get()
+        return data.message
+      }
+    }
+  }
+
+  await testServer([sessionMiddleware, Handler], async url => {
+    const postResponse = await got.post(url, {
+      cookieJar
+    })
+    t.is(postResponse.body, 'OK')
+
+    const getResponse = await got(url, {
+      cookieJar,
+      retry: 0
+    })
+    t.is(getResponse.body, 'Hello, World!')
+  })
+})
+
+test('session data will not be saved if it is null', async t => {})
+
+test('session#update sets data', async t => {})
+
+test('session#destroy resets sid and discard data', async t => {})
+
+test('session#regenerate resets sid and destroy data of previous sid', async t => {})
+
+test('session#regenerate sets data for new sid', async t => {})
